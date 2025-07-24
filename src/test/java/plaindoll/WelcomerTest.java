38f4527d10fde8c4862294ea63c77f4fe67a6d6f
Sergey Puzin <s.v.puzin@gmail.com>
2025-07-24 19:25:46 +0600

    1

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,4 +30,8 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void welcomerSaysHunter2(){
+		assertThat(welcomer.sayHunter(), containsString("hunter"));
+	}
 }
